a598a5475b2a71876fc23ea71cc7e768d895709f
renato aires <rentonaires@gmail.com>
2021-11-17 20:49:17 -0300

    template views interpolacao

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone #importe
+from .models import Post #importe
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #queryset
+    return render(request, 'blog/post_list.html', {'posts': posts})
