91a90da08495deef764335f01a9079044c420b4b
Renato Aires <rentonaires@gmail.com>
2021-12-14 20:59:51 -0300

    criando formulario

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.utils import timezone #importe
 from .models import Post #importe
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect #importe
+from .forms import PostForm
+
 
 # Create your views here.
 
@@ -9,7 +11,24 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #queryset
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+#funcao que retorna o template html
 def post_detail(request, pk): 
-    posts = get_object_or_404(Post, pk=pk) #queryset
-    return render(request, 'blog/post_detail.html', {'posts': posts})
+    post = get_object_or_404(Post, pk=pk) #queryset
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":#condiçao para preencer o fromulario
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)#salva mas nao cria o objeto
+            post.author = request.user#salvando usuario atula 
+            post.published_date = timezone.now()#slava com data e hora atual
+            post.save() #salvando tudo!!!
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html',{'form': form})
+
+
+
 
