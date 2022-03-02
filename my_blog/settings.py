5fb852f961df17ae02b2400ac72c214b35b94e58
Renato Aires <rentonaires@gmail.com>
2022-03-02 19:25:01 -0300

    commit teste 02

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -14,7 +14,7 @@ import os
 
 from decouple import config #comentar aqui!!!
 #from pathlib import Path
-import  dj_database_url
+#import  dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
