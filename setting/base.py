9913503a8123f8e64d342555bb47b40fa134e945
Renato Aires <rentonaires@gmail.com>
2022-03-03 20:01:07 -0300

    adicionar arquivos para deploy no heroku 01

--- setting/base.py
+++ setting/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,3 +144,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') #caminho para arquivos estaticos html css
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- setting/heroku.py
+++ setting/heroku.py
@@ -1,12 +1,12 @@
 import environ  #carregando a blibioteca eviron
 
-from djangogirls2.settings.base import * #
+from my_blog.settings.base import * # importanto o setting base para configurar alguma variaveis
 
 env = environ.Env()
 
 DEBUG = env.bool("DEBUG", False)
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY") #chave de segurando para deploy
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
